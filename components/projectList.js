6d4f9ab864064dc2c18646c8d73e5e704a9d1c6b
Mike Buyanov <rb@ray-bit.ru>
2018-05-12 17:28:21 +0300

    disable sharp module

--- components/projectList.js
+++ components/projectList.js
@@ -4,24 +4,24 @@ const sharp = require('sharp');
 const glob = require('glob');
 
 projectsList.forEach(element => {
-  let initialImages = fs.readdirSync('./public/img/projects/' + element.seoUrl + '/initial/', (err, items) => {
-    if (err) throw err;
-    return items;
-  });
+  // let initialImages = fs.readdirSync('./public/img/projects/' + element.seoUrl + '/initial/', (err, items) => {
+  //   if (err) throw err;
+  //   return items;
+  // });
 
-  initialImages.forEach(img => {
-    sharp('./public/img/projects/' + element.seoUrl + '/initial/' + img)
-      .rotate()
-      .resize(800, 800)
-      .max()
-      .withoutEnlargement()
-      .toFile('./public/img/projects/' + element.seoUrl + '/' + img, function(err) {
-        if (err) {
-          console.log(err);
-          throw err;
-        }
-      });
-  });
+  // initialImages.forEach(img => {
+  //   sharp('./public/img/projects/' + element.seoUrl + '/initial/' + img)
+  //     .rotate()
+  //     .resize(800, 800)
+  //     .max()
+  //     .withoutEnlargement()
+  //     .toFile('./public/img/projects/' + element.seoUrl + '/' + img, function(err) {
+  //       if (err) {
+  //         console.log(err);
+  //         throw err;
+  //       }
+  //     });
+  // });
   let images = glob('./public/img/projects/' + element.seoUrl + '/*.+(jpg|JPG)', (err, items) => {
     if (err) throw err;
     if (items.length > 0) {
