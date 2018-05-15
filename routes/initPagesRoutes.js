a0f630aa4bbdea6452f2ff4b0809707b248a6b5a
Mike Buyanov <rb@ray-bit.ru>
2018-05-15 07:56:43 +0300

    fix domain

--- routes/initPagesRoutes.js
+++ routes/initPagesRoutes.js
@@ -39,8 +39,8 @@ function initPagesRoutes(app) {
   Promise.all([db.getCities(), db.getDeliveryAddresses()]).then(data => {
     const cities = data[0];
     const deliveryAddresses = data[1];
-    const domain = 'localhost:4400';
-    // const domain = 'contecstroy.ru';
+    // const domain = 'localhost:4400';
+    const domain = 'contecstroy.ru';
     const fromEmail = 'rb@ray-bit.ru';
     const toEmail = 'rb@ray-bit.ru';
 
